1b36af6f3fd76ba9ca049dbd08df358254e88690
George Kibe <georgekibewambui@gmail.com>
2022-03-05 12:08:14 +0300

    implementing simple rest framework api

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #custom
+    'rest_framework',
+    'corsheaders',
     #apps
     'base',
     'signalstest'
@@ -34,6 +36,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +122,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 JAZZMIN_SETTINGS = {
     "site_header": "Discord Clone Admin Section",
